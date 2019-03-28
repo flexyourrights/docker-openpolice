da7a9237bcb7febecf25941e9215b07e7a9aef04
Morgan Lesko <wikiworldorder@protonmail.com>
2019-03-28 11:47:10 -0400

    Install Process Worked

--- bin/openpolice-install-4.sh
+++ bin/openpolice-install-4.sh
@@ -13,19 +13,20 @@ docker-compose exec app sed -i 's/SurvLoop\\SurvLoopServiceProvider::class,/Surv
 docker-compose exec app sed -i 's/Illuminate\\Support\\Facades\\View::class,/Illuminate\\Support\\Facades\\View::class,\n\n       "SurvLoop" \=\> WikiWorldOrder\\SurvLoop\\SurvLoopFacade::class,/g' /var/www/config/app.php
 docker-compose exec app sed -i 's/WikiWorldOrder\\SurvLoop\\SurvLoopFacade::class,/WikiWorldOrder\\SurvLoop\\SurvLoopFacade::class,\n       "OpenPolice" \=\> FlexYourRights\\OpenPolice\\OpenPoliceFacade::class,/g' /var/www/config/app.php
 
+docker-compose exec app cp /var/www/vendor/wikiworldorder/survloop/src/Models/User.php /var/www/app/User.php
+docker-compose exec app cp /var/www/vendor/wikiworldorder/survloop/src/Controllers/Middleware/routes-api.php /var/www/routes/api.php
+
 docker-compose exec app php artisan config:clear
 docker-compose exec app php artisan cache:clear
 docker-compose exec app php artisan route:clear
 docker-compose exec app composer dump-autoload
 docker-compose exec app php artisan vendor:publish
 
-docker-compose exec app cp /var/www/vendor/wikiworldorder/survloop/src/Models/User.php /var/www/app/User.php
-docker-compose exec app cp /var/www/vendor/wikiworldorder/survloop/src/Controllers/Middleware/routes-api.php /var/www/routes/api.php
-
 docker-compose exec app php artisan migrate
-docker-compose exec app php artisan optimize
-docker-compose exec app composer dump-autoload
 docker-compose exec app php artisan db:seed --class=SurvLoopSeeder
 docker-compose exec app php artisan db:seed --class=ZipCodeSeeder
 docker-compose exec app php artisan db:seed --class=OpenPoliceSeeder
 docker-compose exec app php artisan db:seed --class=OpenPoliceDeptSeeder
+
+docker-compose exec app php artisan optimize
+docker-compose exec app composer dump-autoload
